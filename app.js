6423bb1ae87bb869ca72580ac3b46d0f31f1e043
byeolingzchoi <byeolingzchoi@gmail.com>
2026-03-18 13:54:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -368,7 +368,6 @@ if (cur) {
   document.addEventListener('mousemove', e => {
     cur.style.left = e.clientX + 'px';
     cur.style.top  = e.clientY + 'px';
-    cur.classList.add('active');
   });
   document.addEventListener('mouseover', e => {
     const el = e.target.closest('a, button, [onclick], .proj-slider, .s-arrow, .proj-detail-close');
